c682b0be04eb83f7f77fccba2b98ce3b8a3e6068
gk-svg <gk32239@gmail.com>
2020-06-26 22:10:45 +0530

    about template added

--- mySchool/urls.py
+++ mySchool/urls.py
@@ -17,5 +17,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('',views.home,name='Home')
+    path('',views.home,name='Home'),
+    path('about/',views.about,name='About')
 ]
--- mySchool/views.py
+++ mySchool/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def home(request):
     return render(request,'mySchool/home.html')
+
+
+def about(request):
+    return render(request,'mySchool/about.html')
