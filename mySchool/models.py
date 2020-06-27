8a91cf912022d8ffb8f71b2ea66343b362060bf7
gk-svg <gk32239@gmail.com>
2020-06-28 00:56:46 +0530

    students fees paymrnt model created

--- mySchool/models.py
+++ mySchool/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Student(models.Model):
     name=models.CharField(max_length=100)
     rollNo=models.CharField(max_length=10)
-    Class=models.CharField(default='')
+    Class=models.CharField(max_length=2, default='')
     father=models.CharField(max_length=120)
     mobileNo=models.CharField(max_length=10)
     fees=models.IntegerField(default=0)
@@ -17,4 +17,11 @@ class Student(models.Model):
 
     def __str__(self):
         return self.name
+class StudentFees(models.Model):
+    name=models.CharField(max_length=100,default='')
+    roll=models.CharField(max_length=10,default='')
+    payment=models.IntegerField(default=0)
+    transaction_id=models.CharField(max_length=100,null=True)
+    def __str__(self):
+        return self.roll
     
